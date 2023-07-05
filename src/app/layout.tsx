dbd5c571c28a0ac76c2792914b770508f401b79d
vishalshukla <vishal.shukla@virtueanalytics.com>
2023-07-05 17:55:12 +0530

    changes client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,10 +8,10 @@ import ChatbotWrapper from "@/components/chatbot/ChatbotWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "House Tax Calculator",
-  description: "House Tax Calculator | Virtue Analytics",
-};
+// export const metadata = {
+//   title: "House Tax Calculator",
+//   description: "House Tax Calculator | Virtue Analytics",
+// };
 
 export default function RootLayout({
   children,
@@ -20,6 +20,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>House Tax Calculator</title>
+        <meta
+          name="description"
+          content="House Tax Calculator by Virtue Analytics"
+        />
+      </head>
       <body className={`${inter.className} bg-slate-200 text-sm`}>
         <Navigation />
         {children}
